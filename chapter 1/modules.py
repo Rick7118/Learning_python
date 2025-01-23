91f8e8ac54f9fdebdc8c8f173d02075133b2edef
Rick <155312472+Rick7118@users.noreply.github.com>
2025-01-23 23:14:48 +0530

    Add files via upload

---
+++ chapter 1/modules.py
@@ -0,0 +1,9 @@
+import pyjokes
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("I will lead a very good life")
+engine.runAndWait()
+
+#use pip to install modules in python and then use import to use them in your code
+
